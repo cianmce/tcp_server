12c4f913351fbd0ed0545116c08c247b0eedad62
Cian McElhinney <cianmce@gmail.com>
2015-10-26 16:23:53 +0000

    ip

--- app.rb
+++ app.rb
@@ -16,6 +16,10 @@ logger.formatter = proc do |severity, datetime, progname, msg|
 end
 logger.info 'made logger'
 
+require 'open-uri'
+remote_ip = open('http://whatismyip.akamai.com').read
+logger.info "remote_ip: #{remote_ip}"
+
 # Make server
 server = Server.new(logger, STUDENT_ID)
 server.run(port_number, MAX_THREADS)
